68717dd099703fc52d2019ddd454935ee7e34fcc
Sulabh <sulabhambule@gmail.com>
2025-07-25 12:15:15 +0530

    Add selection tools and color picker for enhanced layer manipulation

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -31,6 +31,7 @@ import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 200;
 
@@ -99,6 +100,44 @@ export const Canvas = ({
   },
     [lastUsedColor]);
 
+  const translateSelectedLayers = useMutation((
+    { storage, self },
+    point: Point
+  ) => {
+    if (canvasState.mode != CanvasMode.Translating) {
+      return;
+    }
+
+    const offset = {
+      x: point.x - canvasState.current.x,
+      y: point.y - canvasState.current.y,
+    }
+
+    const liveLayers = storage.get("layers");
+
+
+    for (const id of self.presence.selection) {
+      const layer = liveLayers.get(id);
+
+      if (layer) {
+        layer.update({
+          x: layer.get("x") + offset.x,
+          y: layer.get("y") + offset.y,
+        });
+      }
+    }
+
+    setCanvasState(
+      {
+        mode: CanvasMode.Translating,
+        current: point
+      }
+    );
+
+  }, [
+    canvasState,
+  ])
+
   const resizeSelecetedLayer = useMutation((
     { storage, self },
     point: Point,
@@ -121,6 +160,14 @@ export const Canvas = ({
     }
 
   }, [canvasState])
+
+  const unselectLayers = useMutation((
+    { self, setMyPresence }
+  ) => {
+    if (self.presence.selection.length > 0) {
+      setMyPresence({ selection: [] }, { addToHistory: true });
+    }
+  }, [])
 
   const onResizeHandlePointerDown = useCallback((
     corner: Side,
@@ -155,7 +202,9 @@ export const Canvas = ({
 
     const current = pointerEventToCanvasPoint(e, camera);
 
-    if (canvasState.mode === CanvasMode.Resizing) {
+    if (canvasState.mode == CanvasMode.Translating) {
+      translateSelectedLayers(current);
+    } else if (canvasState.mode === CanvasMode.Resizing) {
       resizeSelecetedLayer(current);
     }
 
@@ -164,8 +213,8 @@ export const Canvas = ({
     canvasState,
     resizeSelecetedLayer,
     camera,
-    
-  ])
+    translateSelectedLayers
+  ]);
 
   const onPointerLeave = useMutation((
     { setMyPresence }
@@ -173,18 +222,43 @@ export const Canvas = ({
     setMyPresence({ cursor: null });
   }, []);
 
+  const onPointerDown = useCallback((
+    e: React.PointerEvent,
+  ) => {
+    const point = pointerEventToCanvasPoint(e, camera);
+    if (canvasState.mode === CanvasMode.Inserting) {
+      return;
+    }
+
+    // TODO add case for the drawing.
+
+    setCanvasState({ origin: point, mode: CanvasMode.Pressing });
+  }, [
+    camera,
+    canvasState.mode,
+    setCanvasState
+  ]);
+
   const onPointerUp = useMutation((
     { },
     e
   ) => {
     const point = pointerEventToCanvasPoint(e, camera);
 
-    if (canvasState.mode === CanvasMode.Inserting) {
+    if (
+      canvasState.mode === CanvasMode.None ||
+      canvasState.mode === CanvasMode.Pressing
+    ) {
+      unselectLayers();
+      setCanvasState({
+        mode: CanvasMode.None,
+      });
+    } else if (canvasState.mode === CanvasMode.Inserting) {
       insertLayer(canvasState.layerType, point);
     } else {
       setCanvasState({
         mode: CanvasMode.None,
-      })
+      });
     }
 
     history.resume();
@@ -194,7 +268,8 @@ export const Canvas = ({
     history,
     insertLayer,
     setCanvasState,
-  ])
+    unselectLayers
+  ]);
 
   const selections = useOthersMapped((other) => other.presence.selection);
 
@@ -259,12 +334,18 @@ export const Canvas = ({
         redo={history.redo}
       />
 
+      <SelectionTools
+        camera={camera}
+        setLastUsedColor={setLastUsedColor}
+      />
+
       <svg
         className="h-[100vh] w-[100vw]"
         onWheel={onWheel}
         onPointerMove={onPointerMove}
         onPointerLeave={onPointerLeave}
         onPointerUp={onPointerUp}
+        onPointerDown={onPointerDown}
       >
         <g
           style={{
