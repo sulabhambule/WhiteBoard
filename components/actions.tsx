6b7b8c3e879131583c9cafd0b15bcd97c46a48a7
Sulabh <sulabhambule@gmail.com>
2025-07-26 23:13:49 +0530

    Fix link formatting in copy link function

--- components/actions.tsx
+++ components/actions.tsx
@@ -38,7 +38,7 @@ export const Actions = ({
 
   const onCopyLink = () => {
     navigator.clipboard.writeText(
-      `${window.location.origin} / board / ${id}`,
+      `${window.location.origin}/board/${id}`,
     )
       .then(() => toast.success("Link copied"))
       .catch(() => toast.error("Failed to copy link"))
